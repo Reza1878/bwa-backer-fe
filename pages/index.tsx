3f3fecb443eb03f7cd11be5bd3581361b319fc1a
Reza Rizqi Ramdhani <rezarizqir@gmail.com>
2022-07-31 16:31:50 +0700

    Update: step section

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,9 +13,9 @@ const Home: NextPage = () => {
         <title>Bwa Backer</title>
       </Head>
       <div>
-        <div className="header__bg lg:max-h-[660px] -skew-y-6 origin-top-left h-auto" />
-        <header className="relative md:pt-5 pt-0">
-          <nav className="container px-6 mx-auto md:px-24 py-4 flex justify-between flex-wrap flex-col lg:flex-row">
+        <div className="header__bg lg:max-h-[664px] -skew-y-6 origin-top-left h-auto" />
+        <header className="relative my-6">
+          <nav className="container px-6 xl:px-24 mx-auto py-4 flex justify-between flex-wrap flex-col lg:flex-row">
             <div className="flex items-center flex-wrap">
               <div className="flex justify-between w-full lg:w-auto items-center">
                 <Image
@@ -77,15 +77,15 @@ const Home: NextPage = () => {
               </a>
             </div>
           </nav>
-          <div className="container px-6 mx-auto flex flex-wrap justify-between md:px-24">
+          <div className="container px-6 xl:px-24 mx-auto flex flex-wrap justify-between pt-6">
             <div className="lg:w-1/2 w-full">
-              <h1 className="text-white font-medium text-4xl mb-8">
+              <h1 className="text-white font-medium text-2xl lg:text-4xl mb-8">
                 We helps <u className="decoration-secondary">startup</u>
                 <br className="block" /> to getting started &{" "}
                 <u className="decoration-secondary">funding</u>
                 <br className="block" /> their truly needs
               </h1>
-              <p className="text-white text-xl font-light mb-8">
+              <p className="text-white text-base lg:text-xl font-light mb-8">
                 Fund the best idea to become <br />a real product and be the
                 contributor
               </p>
@@ -102,6 +102,50 @@ const Home: NextPage = () => {
             </picture>
           </div>
         </header>
+
+        <main className="lg:pt-24 2xl:pt-0">
+          <section id="step-section" className="container px-6 mx-auto">
+            <h2 className="text-2xl">
+              Only 3 steps to execute your brigth ideas
+            </h2>
+            <div className="flex items-center flex-col">
+              <picture className="lg:block hidden">
+                <img alt="line-step" src="/image/line-step.svg" />
+              </picture>
+              <div className="w-full flex justify-evenly flex-wrap">
+                <div className="text-center w-full lg:w-1/3 flex flex-col items-center lg:py-0 py-6">
+                  <picture>
+                    <img src="/image/step-1-illustration.svg" alt="Step 1" />
+                  </picture>
+                  <p className="py-2 font-medium text-xl">Sign Up</p>
+                  <p>
+                    Sign Up account and start <br /> funding project{" "}
+                  </p>
+                </div>
+                <div className="text-center w-full lg:w-1/3 flex flex-col items-center lg:-mt-14 lg:py-0 py-6">
+                  <picture>
+                    <img src="/image/step-2-illustration.svg" alt="Step 1" />
+                    <p className="py-2 font-medium text-xl">Open Project</p>
+                    <p>
+                      Choose some project idea, <br />
+                      and start funding
+                    </p>
+                  </picture>
+                </div>
+                <div className="text-center w-full lg:w-1/3 flex flex-col items-center lg:-mt-28 lg:py-0 py-6">
+                  <picture>
+                    <img src="/image/step-3-illustration.svg" alt="Step 1" />
+                  </picture>
+                  <p className="py-2 font-medium text-xl">Execute</p>
+                  <p>
+                    Time to makes dream <br />
+                    comes true
+                  </p>
+                </div>
+              </div>
+            </div>
+          </section>
+        </main>
       </div>
     </div>
   );
