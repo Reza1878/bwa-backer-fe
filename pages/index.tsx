dc5937baf0ebb754764e719418dc1319ac9ef3df
Reza Rizqi Ramdhani <rezarizqir@gmail.com>
2022-08-03 17:31:42 +0700

    Update: Happy client section

--- pages/index.tsx
+++ pages/index.tsx
@@ -215,7 +215,7 @@ const Home: NextPage = () => {
                   <div className="w-full">
                     <picture>
                       <img
-                        className="rounded-sm w-full h-52 object-cover"
+                        className="rounded-sm w-full lg:h-52 object-cover"
                         src={`/image/${project.image}`}
                         alt=""
                       />
@@ -254,6 +254,53 @@ const Home: NextPage = () => {
                   </div>
                 </div>
               ))}
+            </div>
+          </section>
+
+          <section className="container px-6 xl:px-24 mx-auto py-4">
+            <h2 className="text-2xl lg:text-3xl">
+              See What Out <br className="hidden lg:block" />
+              Happy Clients Say
+            </h2>
+
+            <div className="flex pt-6 lg:pl-20">
+              <picture>
+                <img
+                  className="hidden lg:block"
+                  src="/image/testimonial-line.svg"
+                  alt=""
+                />
+              </picture>
+              <div className="lg:p-8 w-full lg:w-1/2">
+                <h1 className="text-2xl leading-relaxed">
+                  <i>&quot; </i>
+                  Funding at Bucker is very easy and comfortable. Just need to
+                  find an idea, click and already funding.
+                  <i> &quot;</i>
+                </h1>
+                <div className="pt-2 text-center lg:text-left">
+                  <p className="text-lg">Sophie Nicole</p>
+                  <p className="font-light text-base text-gray-400">
+                    Project Manager
+                  </p>
+                </div>
+
+                <div className="flex pt-4 justify-center lg:justify-start">
+                  <picture className="opacity-60 pr-4">
+                    <img src="/image/testimonial-1-icon.png" alt="" />
+                  </picture>
+                  <picture className="opacity-60 pr-4">
+                    <img src="/image/testimonial-2-icon.png" alt="" />
+                  </picture>
+                  <picture className="pr-4">
+                    <img
+                      src="/image/testimonial-3-icon.png"
+                      alt=""
+                      className="rounded-full border-2 border-primary p-1"
+                    />
+                  </picture>
+                </div>
+              </div>
             </div>
           </section>
         </main>
